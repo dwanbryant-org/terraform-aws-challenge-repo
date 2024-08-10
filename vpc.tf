9d27088f4343386567f83828f7e6aae00962e200
Dwan Bryant <_Dwan.Bryant.AZ@sodexo.com>
2024-08-10 18:52:28 -0400

    removed accounts.tf and added key

--- vpc.tf
+++ vpc.tf
@@ -6,6 +6,41 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+resource "aws_kms_key" "cloudwatch_key" {
+  description             = "KMS key for CloudWatch log group encryption"
+  enable_key_rotation     = true
+
+  policy = data.aws_iam_policy_document.default_key_policy.json
+
+  tags = {
+    Name = "cloudwatch-key"
+  }
+}
+
+data "aws_iam_policy_document" "default_key_policy" {
+  statement {
+    actions = [
+      "kms:Encrypt",
+      "kms:Decrypt",
+      "kms:ReEncrypt*",
+      "kms:GenerateDataKey*",
+      "kms:DescribeKey",
+    ]
+
+    principals {
+      type        = "AWS"
+      identifiers = [
+        # Add the ARN of the IAM role, user, or service that should be allowed to use the key
+        "arn:aws:iam::${var.account_number}:role/cicd",
+      ]
+    }
+
+    resources = [
+      "*"
+    ]
+  }
+}
+
 module "vpc_nfw" {
   source =  "github.com/Coalfire-CF/terraform-aws-vpc-nfw"
 
@@ -40,7 +75,7 @@ module "vpc_nfw" {
 
   flow_log_destination_type = "cloud-watch-logs"
   cloudwatch_log_group_retention_in_days = 30
-  cloudwatch_log_group_kms_key_id        = data.terraform_remote_state.day0.outputs.cloudwatch_kms_key_arn
+  cloudwatch_log_group_kms_key_id        = aws_kms_key.cloudwatch_key.arn
 
   tags = {
     Owner       = var.resource_prefix
