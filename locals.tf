8f5b69f9a04d8353eca4ba25e737bbc930538f23
Dwan Bryant <_Dwan.Bryant.AZ@sodexo.com>
2024-08-10 19:14:58 -0400

    changed from map to list

--- locals.tf
+++ locals.tf
@@ -1,11 +1,4 @@
 locals {
-   public_subnets = {
-    "subnet1" = "10.1.0.0/24",
-    "subnet2" = "10.1.1.0/24"
-  }
-
-  private_subnets = {
-    "subnet3" = "10.1.2.0/24",
-    "subnet4" = "10.1.3.0/24"
-  }
+    public_subnets = ["10.1.0.0/24", "10.1.1.0/24"]
+    private_subnets = ["10.1.2.0/24", "10.1.3.0/24"]
 }
