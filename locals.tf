db037f8276fca7141551d5665a909824100a31f4
Dwan Bryant <_Dwan.Bryant.AZ@sodexo.com>
2024-08-10 19:20:20 -0400

    changed locals

--- locals.tf
+++ locals.tf
@@ -1,4 +1,10 @@
 locals {
-    public_subnets = ["10.1.0.0/24", "10.1.1.0/24"]
-    private_subnets = ["10.1.2.0/24", "10.1.3.0/24"]
+    all_subnets = {
+        "subnet-public-1" = "10.1.0.0/24",
+        "subnet-public-2" = "10.1.1.0/24",
+        "subnet-private-1" = "10.1.2.0/24",
+        "subnet-private-1" = "10.1.3.0/24"
+    }
+    public_subnets   = [for k, v in local.all_subnets: v if length(regexall(".*public.*", k)) > 0]
+    private_subnets  = [for k, v in local.all_subnets : v if(length(regexall(".*priv.*", k)) > 0 || length(regexall(".*compute.*", k)) > 0)]
 }
