8d8e001b08a2c3d27c5806cbba147f0c92eef86b
Dwan Bryant <_Dwan.Bryant.AZ@sodexo.com>
2024-08-10 03:50:45 -0400

    changed bucket region

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket         = "bryant-terraform-tfstate"
     key            = "terraform.tfstate"
-    region         =  "us-east-1"
+    region         =  "us-east-2"
     dynamodb_table = "bryant-terraform-tfstate-db"
   }
 }
