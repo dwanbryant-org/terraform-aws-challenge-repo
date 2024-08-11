31ec00aef1f6b15bcbd595108da834f8cc95b7ce
Dwan Bryant <_Dwan.Bryant.AZ@sodexo.com>
2024-08-11 17:20:10 -0400

    added s3 buckets

---
+++ s3.tf
@@ -0,0 +1,71 @@
+module "images_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.1.2"
+
+  bucket = "images"
+
+  lifecycle_rules = [
+    {
+      id      = "archive-memes"
+      enabled = true
+
+      filter = {
+        prefix = "Memes/"
+      }
+
+      transitions = [
+        {
+          days          = 90
+          storage_class = "GLACIER"
+        }
+      ]
+    }
+  ]
+
+  tags = {
+    Name        = "images"
+    Environment = "coalfire"
+  }
+}
+
+module "logs_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.1.2"
+
+  bucket = "logs"
+
+  lifecycle_rules = [
+    {
+      id      = "archive-active"
+      enabled = true
+
+      filter = {
+        prefix = "Active/"
+      }
+
+      transitions = [
+        {
+          days          = 90
+          storage_class = "GLACIER"
+        }
+      ]
+    },
+    {
+      id      = "delete-inactive"
+      enabled = true
+
+      filter = {
+        prefix = "Inactive/"
+      }
+
+      expiration = {
+        days = 90
+      }
+    }
+  ]
+
+  tags = {
+    Name        = "logs"
+    Environment = "coalfire"
+  }
+}
