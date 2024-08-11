27fd9de026f4406b78add13dbddc429563a1dbab
dwanbryant <122132781+dwanbryant@users.noreply.github.com>
2024-08-11 18:56:26 -0400

    added s3 buckets (#6)
    * added s3 buckets
    
    * added s3
    
    * fixed s3
    
    * fixed
    
    * fixed
    
    * fixed
    
    * fixed
    
    * add iam roles
    
    * changed bucket arn
    
    * changed bucket arn
    
    * added rest
    
    * fixed
    
    * encoded userdata
    
    * used vpc id instead of name
    
    * added instance profile
    
    * added documentation
    
    ---------
    
    Co-authored-by: Dwan Bryant <_Dwan.Bryant.AZ@sodexo.com>

---
+++ s3.tf
@@ -0,0 +1,71 @@
+module "images_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.1.2"
+
+  bucket = "bryant-coalfire-images"
+
+  lifecycle_rule = [
+    {
+      id      = "archive-memes"
+      enabled = true
+
+      filter = {
+        prefix = "Memes/"
+      }
+
+      transition = [
+        {
+          days          = 90
+          storage_class = "GLACIER"
+        }
+      ]
+    }
+  ]
+
+  tags = {
+    Name        = "images"
+    Environment = "coalfire"
+  }
+}
+
+module "logs_bucket" {
+  source  = "terraform-aws-modules/s3-bucket/aws"
+  version = "4.1.2"
+
+  bucket = "bryant-coalfire-logs"
+
+  lifecycle_rule = [
+    {
+      id      = "archive-active"
+      enabled = true
+
+      filter = {
+        prefix = "Active/"
+      }
+
+      transition = [
+        {
+          days          = 90
+          storage_class = "GLACIER"
+        }
+      ]
+    },
+    {
+      id      = "delete-inactive"
+      enabled = true
+
+      filter = {
+        prefix = "Inactive/"
+      }
+
+      expiration = {
+        days = 90
+      }
+    }
+  ]
+
+  tags = {
+    Name        = "logs"
+    Environment = "coalfire"
+  }
+}
